57647577402032abf9ae59279f4f7da4b6f23abb
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-07-10 14:11:01 -0300

    Adiciona resolução do desafio do Codelab 01.3 - Scrolling Text

--- app/src/main/java/com/estudo/rolagemdetexto/MainActivity.java
+++ app/src/main/java/com/estudo/rolagemdetexto/MainActivity.java
@@ -3,6 +3,9 @@ package com.estudo.rolagemdetexto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button add_comment = findViewById(R.id.add_comment);
+        add_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
